06632ea3fa9132301ef6f6843a5273ecee6ef83a
cristianlana12 <cristian56cordoba@gmail.com>
2026-07-17 13:34:33 -0300

    usando y apuntando uso de left join con merge de pandas

---
+++ MergeAndJoin.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+# 1. Tabla Izquierda (Transaccional)
+ventas = pd.DataFrame({
+    'id_venta': [101, 102, 103],
+    'id_cliente': ['C1', 'C2', 'C99'], # Ojo: C99 no existe en la tabla clientes
+    'total': [1500, 2300, 900]
+})
+
+# 2. Tabla Derecha (Catálogo/Dimensional)
+clientes = pd.DataFrame({
+    'id_cliente': ['C1', 'C2', 'C3'], # C3 no compró nada
+    'nombre': ['Ana', 'Juan', 'Pedro'],
+    'ciudad': ['Madrid', 'Barcelona', 'Valencia']
+})
+
+## left join
+
+df_final = pd.merge(
+    left=ventas,
+    right=clientes,
+    on='id_cliente',  ##fk usada como referenciador
+    how='left'  ##left join nos quedamos con la tabla de la izquierda y las coincidencias entre izq y der
+)
+
+print(df_final)
+
+''' 
+hace la inteccion, y nos quedamos con los datos puros de ventas como tal, al c3 no coincidir con los id_clientes de venta no lo agrega 
+completa con NaN los datos faltantes de la venta de c99
+
+      id_venta  id_cliente  total  nombre     ciudad
+0       101         C1      1500    Ana       Madrid
+1       102         C2      2300    Juan     Barcelona
+2       103        C99      900     NaN        NaN
+'''

70629f306848435607c2e3a8169ad31b98e6e451
cristianlana12 <cristian56cordoba@gmail.com>
2026-08-06 12:04:50 -0300

    normalizacion

--- TecnicaNormalizacionDatos.py
+++ TecnicaNormalizacionDatos.py
@@ -2,3 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+df = sns.load_dataset("iris")
+
+std_scaler = StandardScaler()
+df_std_scaler = std_scaler.fit_transform(df.iloc[:,:-1])
+print(df_std_scaler)
+
+mm_scaler = MinMaxScaler()
+df_normalized = mm_scaler.fit_transform(df.iloc[:,:-1])
+print(df_normalized)
